a77dd214d0aef9eb1614549226e923165c47cd03
Collin Brittle <cnbrittle@gmail.com>
2015-11-21 20:12:03 -0500

    Add new options, expand prohibited characters.
    The new options are length, whitespace, and replace.
    Length is how long the name should be truncated to, if it is too long.
    Padding is subtracted from the length. Whitespace is a string that will
    replace any whitespace characters after they have been stripped from the
    beginning and end of the string. Multiple whitespace characters are
    still combined into one. Replace is a string that will replace
    prohibited characters rather than deleting them.

--- lib/zaru.rb
+++ lib/zaru.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
-
 class Zaru
-  CHARACTER_FILTER = /[\x00-\x1F\/\\:\*\?\"<>\|]/u
+  CHARACTER_FILTER = /[\p{Cntrl}\[\]\{\}\|\/\\`!@$*()<>?'";:]/u
   UNICODE_WHITESPACE = /[[:space:]]+/u
   WINDOWS_RESERVED_NAMES =
     %w{CON PRN AUX NUL COM1 COM2 COM3 COM4 COM5
@@ -9,15 +8,18 @@ class Zaru
        LPT5 LPT6 LPT7 LPT8 LPT9}
   FALLBACK_FILENAME = 'file'
 
-  def initialize(filename, options = {})
+  def initialize(filename, options={})
     @padding = options[:padding] || 0
+    @length = options[:length] || 255
+    @whitespace = options[:whitespace] || ' '
+    @replacement_char = options[:replace] || ''
     @raw = filename.to_s.freeze
   end
 
   # strip whitespace on beginning and end
   # collapse intra-string whitespace into single spaces
   def normalize
-    @normalized ||= @raw.strip.gsub(UNICODE_WHITESPACE,' ')
+    @raw.strip.gsub(UNICODE_WHITESPACE, @whitespace)
   end
 
   # remove bad things!
@@ -26,15 +28,14 @@ class Zaru
   # - don't allow filenames to start with a dot
   # - don't allow empty filenames
   def sanitize
-    @sanitized ||=
-      filter(normalize.gsub(CHARACTER_FILTER,''))
+    filter(normalize)
   end
 
   # cut off at 255 characters
   # optionally provide a padding, which is useful to
   # make sure there is room to add a file extension later
   def truncate
-    @truncated ||= sanitize.chars.to_a.slice(0..254-@padding).join
+    sanitize.slice(0, @length-@padding)
   end
 
   def to_s
@@ -42,28 +43,33 @@ class Zaru
   end
 
   # convenience method
-  def self.sanitize!(filename, options = {})
+  def self.sanitize!(filename, options={})
     new(filename, options).to_s
   end
 
   private
 
     def filter(filename)
+      filename = filter_characters(filename)
       filename = filter_windows_reserved_names(filename)
       filename = filter_blank(filename)
-      filename = filter_dot(filename)
+      filename = filter_start(filename)
+    end
+
+    def filter_characters(filename)
+      filename.gsub(CHARACTER_FILTER, @replacement_char)
     end
 
     def filter_windows_reserved_names(filename)
-      WINDOWS_RESERVED_NAMES.include?(filename.upcase) ? 'file' : filename
+      WINDOWS_RESERVED_NAMES.include?(filename.upcase) ? FALLBACK_FILENAME : filename
     end
 
     def filter_blank(filename)
-      filename.empty?? FALLBACK_FILENAME : filename
+      filename.empty? ? FALLBACK_FILENAME : filename
     end
 
-    def filter_dot(filename)
-      filename.start_with?('.')? "#{FALLBACK_FILENAME}#{filename}" : filename
+    def filter_start(filename)
+      filename.sub(/^(-[[:space:]]?)+/u, '')
     end
 
 end
